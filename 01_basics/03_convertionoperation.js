94b81d204045cc0664589b71e81059b707dfbd0e
Shibam806 <shibamroy806@gmail.com>
2025-07-17 11:10:17 +0000

    convertion of datatype

---
+++ 01_basics/03_convertionoperation.js
@@ -0,0 +1,30 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let myScore = Number(score)
+console.log(myScore);
+console.log(typeof myScore);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let myLoggedIn = Boolean(isLoggedIn)
+console.log(myLoggedIn)
+
+// 1 => true; 0 => flase
+// "" => false
+// "shibam" => true
+
+let stringNumber = 33
+
+let myStringNumber = String(stringNumber)
+console.log(myStringNumber);
+console.log(typeof (myStringNumber));
+
+
+
