9b276764474c454114b79884d397bd4a8aff7131
Shibam806 <shibamroy806@gmail.com>
2025-07-17 09:42:31 +0000

    dataType

---
+++ 01_basics/test.datatype.js
@@ -0,0 +1,24 @@
+"use strict"// treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+
+let name = "shibam"
+let age = 28
+let isLoggedIn = true
+let state = null
+let city;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// symbol => unique
+
+// object
+
+
+console.log(typeof "shibam");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
