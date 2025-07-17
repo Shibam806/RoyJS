6aaf7eb671a96dc4217f08d6811dd9e59bfe24c4
Shibam806 <shibamroy806@gmail.com>
2025-07-17 19:28:19 +0000

    datatype summary

---
+++ 01_basics/datatype - summary.js
@@ -0,0 +1,51 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Symbol, Null, Undefined, BigInt
+
+const userName  = "Shibam"
+console.log(typeof (userName));
+
+const score = 100
+console.log(typeof (score));
+
+const isLoggedIn = false
+console.log(typeof (isLoggedIn));
+
+const bigNumber = 23758674635242634n
+console.log(typeof (bigNumber));
+
+const outSideTemp = null
+console.log(typeof (outSideTemp));
+
+let userEmail;
+console.log(typeof (userEmail));
+
+const id = Symbol('4070')
+const anotherId = Symbol('4070')
+console.log(id === anotherId);
+
+// Reference (Non - Primitive)
+
+// Array, Objects, Functions
+
+let heroes = ["Hulk", "SpiderMan", "Thanos"]
+
+const myObj = {
+    name : "Shibam",
+    age : 28,
+}
+
+const myFunction = function(){
+    console.log("Hello Google");
+    
+}
+
+console.log(typeof anotherId);
+
+
+
+
+
+
+
+
