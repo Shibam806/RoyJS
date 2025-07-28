92dc757b3c1c0610f8388b1331242048326024b6
Shibam806 <shibamroy806@gmail.com>
2025-07-28 06:26:05 +0000

    num and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,42 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(200)
+// console.log(balance);
+
+const otherNumber = 123.78594
+// console.log(otherNumber.toFixed());
+// console.log(otherNumber.toPrecision(4));
+
+
+const hundred = 10000000
+//console.log(hundred.toLocaleString('en-IN'));
+
+//+++++++++++++++++Math++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-2));
+// console.log(Math.round(5.2));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(1,5,3,8));
+// console.log(Math.max(1,6,7,9,3));
+
+
+console.log(Math.random());// Give random value between 0-1
+
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random()*10) + 1); // by using floor we will get single random value
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
